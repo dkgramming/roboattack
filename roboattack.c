6c24e6fa4f25625cd7a606bb71bc54c3143deab0
DAVID KISLUK <david.kisluk@gmail.com>
2017-12-04 23:06:31 -0500

    Hard code the starting positions for target and robots

--- roboattack.c
+++ roboattack.c
@@ -19,11 +19,26 @@ int main() {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &world);
 
-    struct Vec2 position;
-    position.x = rank;
-    position.y = rank;
+    struct Vec2 target;
+    target.x = 3; target.y = 3;
 
-    printf("I'm at [%i, %i], look at me!\n", position.x, position.y);
+    struct Vec2 pos[6]; // disregard 0-th index
+    pos[0].x = 3;
+    pos[0].y = 4;
+
+    pos[1].x = 6;
+    pos[1].y = 1;
+
+    pos[2].x = 5;
+    pos[2].y = 7;
+
+    pos[3].x = 0;
+    pos[3].y = 6;
+
+    pos[4].x = 2;
+    pos[4].y = 0;
+
+    printf("P%i: (%i, %i) Target: (%i, %i)\n", rank, pos[rank].x, pos[rank].y, target.x, target.y);
 
     MPI_Finalize();
 }
