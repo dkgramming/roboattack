13d5089a8f8c63853fe49172d83a7b7944cbef08
DAVID KISLUK <david.kisluk@gmail.com>
2017-12-05 00:43:40 -0500

    Set up basic send and receive

--- roboattack.c
+++ roboattack.c
@@ -35,6 +35,7 @@ int main() {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &world);
 
+
     struct Vec2 target;
     target.x = 3; target.y = 3;
 
@@ -55,8 +56,29 @@ int main() {
     pos[4].y = 0;
 
     printf("P%i: (%i, %i) Target: (%i, %i)\n", rank, pos[rank].x, pos[rank].y, target.x, target.y);
-    if (isAdjacent(pos[rank], target)) {
-        printf("P%i: Elect me as your leader!\n", rank);
+
+    int data_to_send = (int)isAdjacent(pos[rank], target);
+    int send_count = 1;
+    MPI_Datatype send_type = MPI_INT;
+    int destination_ID = (rank + 1) % world;
+    int send_tag = 42; // arbritrary currently
+    int received_data;
+    int receive_count = send_count;
+    MPI_Datatype receive_type = send_type;
+    int sender_ID = (rank - 1) % world;
+    sender_ID = (sender_ID < 0) ? sender_ID + world : sender_ID; // modulus isn't perfect
+    int receive_tag = send_tag;
+    MPI_Comm comm = MPI_COMM_WORLD;
+    MPI_Status status; // TODO: Figure this guy out
+
+    MPI_Sendrecv(&data_to_send, send_count, send_type, destination_ID, send_tag,
+      &received_data, receive_count, receive_type, sender_ID, receive_tag, 
+      comm, &status);
+
+    if (received_data == 1) {
+        printf("P%i is our lord and savior!\n", sender_ID);
+    } else {
+        printf("P%i is a false prophet!\n", sender_ID);
     }
 
     MPI_Finalize();
